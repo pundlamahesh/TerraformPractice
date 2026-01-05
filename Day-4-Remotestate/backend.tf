4e21b315972ab464e13cefafa558b295af17ef3e
mahesh.pundla@wisetechglobal.com <pundlamahesh@gmail.com>
2026-01-05 17:33:22 +0530

    message

--- Day-4-Remotestate/backend.tf
+++ Day-4-Remotestate/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mahesh-1608"
-    key = "day-4/terraform.tfstate"
+    key = "statelock/terraform.tfstate"
     region = "us-east-1"
     #enable s3 native locking
     use_lockfile = true
