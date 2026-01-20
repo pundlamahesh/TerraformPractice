046fb874f9a1aa97447c84c0c01a1a9632bea0d5
mahesh.pundla@wisetechglobal.com <pundlamahesh@gmail.com>
2026-01-20 22:01:07 +0530

    Day8

---
+++ Day-8-Pass-Values-to-test/main.tf
@@ -0,0 +1,5 @@
+module "test" {
+  source = "../Day-8-Modules"
+  ami_id = "ami-068c0051b15cdb816"
+  type = "t2.nano"
+}
---
+++ Day-8-pass-values-to-modules/main.tf
@@ -0,0 +1,5 @@
+module "Dev" {
+  source = "../Day-8-Modules"
+  ami_id = "ami-068c0051b15cdb816"
+  type = "t2.micro"
+}
