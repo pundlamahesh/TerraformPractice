23c7ca7f24d0b3311c9480b2059f4f481ee1d295
mahesh.pundla@wisetechglobal.com <pundlamahesh@gmail.com>
2026-01-22 17:31:38 +0530

    Day-10 data block

---
+++ Day-10-Datasource/main.tf
@@ -0,0 +1,36 @@
+data "aws_subnet" "name" {
+    filter {
+      name = "tag:Name"
+      values = ["dev"]
+    }
+  
+}
+
+data "aws_ami" "amzlinux" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "name"
+    values = [ "amzn2-ami-hvm-*-gp2" ]
+  }
+             filter {
+    name = "root-device-type"
+    values = [ "ebs" ]
+  }
+        filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
+}
+
+resource "aws_instance" "name" {
+    #ami = "ami-068c0051b15cdb816"
+    ami = data.aws_ami.amzlinux.id
+    instance_type = "t2.micro"
+    subnet_id = data.aws_subnet.name.id
+  
+}
